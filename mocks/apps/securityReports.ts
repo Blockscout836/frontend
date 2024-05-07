392b258c436fac9b6ed57e04b861f38ac4d4ec86
HaiSli <admin@192.168.0.8>
2024-05-07 16:06:16 +0800

    /blockscout/ => /blockscout836/

--- mocks/apps/securityReports.ts
+++ mocks/apps/securityReports.ts
@@ -1,9 +1,9 @@
 export const securityReports = [
   {
-    appName: 'token-approval-tracker',
-    doc: 'http://docs.li.fi/smart-contracts/deployments#mainnet',
+    appName: "token-approval-tracker",
+    doc: "http://docs.li.fi/smart-contracts/deployments#mainnet",
     chainsData: {
-      '1': {
+      "1": {
         overallInfo: {
           verifiedNumber: 1,
           totalContractsNumber: 1,
@@ -20,20 +20,22 @@ export const securityReports = [
         },
         contractsData: [
           {
-            address: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
+            address: "0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE",
             isVerified: true,
             solidityScanReport: {
-              connection_id: '',
-              contract_address: '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
-              contract_chain: 'optimism',
-              contract_platform: 'blockscout',
-              contract_url: 'http://optimism.blockscout.com/address/0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE',
-              contractname: 'LiFiDiamond',
+              connection_id: "",
+              contract_address: "0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE",
+              contract_chain: "optimism",
+              contract_platform: "blockscout",
+              contract_url:
+                "http://optimism.blockscout.com/address/0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE",
+              contractname: "LiFiDiamond",
               is_quick_scan: true,
               node_reference_id: null,
-              request_type: 'threat_scan',
-              scanner_reference_url: 'http://solidityscan.com/quickscan/0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE/blockscout/eth?ref=blockscout',
-              scan_status: 'scan_done',
+              request_type: "threat_scan",
+              scanner_reference_url:
+                "http://solidityscan.com/quickscan/0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE/blockscout836/eth?ref=blockscout",
+              scan_status: "scan_done",
               scan_summary: {
                 issue_severity_distribution: {
                   critical: 0,
@@ -45,9 +47,9 @@ export const securityReports = [
                 },
                 lines_analyzed_count: 72,
                 scan_time_taken: 1,
-                score: '4.38',
-                score_v2: '87.50',
-                threat_score: '100.00',
+                score: "4.38",
+                score_v2: "87.50",
+                threat_score: "100.00",
               },
             },
           },
